827f2e81758d9b62e27e98104044851e8cb6d584
elphie-pro <onazjr2006@gmail.com>
2025-01-10 10:59:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,272 @@
+import './App.css'
+import logo from '../src/assets/unnamed 1.png'
+import arrow from './assets/diagonal-arrow-svgrepo-com.svg'
+import design from './assets/Group 61.png'
+import first from './assets/image.png'
+import second from './assets/image (1).png'
+import third from './assets/image (2).png'
+import fourth from './assets/image (3).png'
+import fifth from './assets/image (4).png'
+import arr from './assets/Arrow 1.png'
+import arro from './assets/Arrow 2.png'
+import wallet from './assets/empty-wallet-add.png'
+import globe from './assets/image 14.png'
+import phone from '../src/assets/Device 14PM.png'
+import empty from './assets/empty-wallet-add.png'
+import monnify from './assets/image 15.png'
+import accross from './assets/image 5.png'
+import power from './assets/image 6.png'
+import prodiy from './assets/image 7.png'
+import stars from './assets/image 8.png'
+import serbit from './assets/image 16.png'
+import vertio from './assets/image 17.png'
+import pay from './assets/image 10.png'
+import providus from './assets/image 11.png'
+import stripe from './assets/image 9.png'
+import smile from './assets/image 18.png'
+import group from './assets/Group 1410103861.png'
+import groupp from  './assets/Group 1410103861 (1).png'
+import star from './assets/Star.png'
+import back from './assets/Group (3).png'
+import frame from './assets/Frame 1707478677.png'
+import starr from './assets/Star (2).png'
+import curly from './assets/Arrow 2 (2).png'
+import minus from './assets/minus.png'
+import plus from './assets/plus.png'
+import contact from './assets/contact.png'
+import ar from './assets/Arrow 3.png'
+import arw from './assets/Arrow 3 (1).png'
+import man from './assets/image (7).png'
+import woman from './assets/image (8).png'
+import payme from './assets/Pay4me.png'
+import app from './assets/Store download button (1).png'
+import google from './assets/Store download button.png'
+import QR from './assets/rb_2915 1.png'
+import logos from './assets/Frame 51.png'
+import phoeee from './assets/Device .png'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <div className="background">
+      <img src={design} alt="" style={{background: 'none', width: '20vw', marginTop: '0%', marginLeft: '75%', position: 'absolute', opacity: '100%', transform: 'rotate(60deg)', filter: 'blur(5px)'}}/>
+      <img src={design} alt="" style={{background: 'none', width: '20vw', marginLeft: '2%', marginTop: '6%', position: 'absolute', opacity: '100%', transform: 'rotate(-60deg)', filter: 'blur(5px)'}}/>
+      </div>
+      <nav>
+        <img src={logo} alt="" className='logo'/>
+        <h3 className='title'> Pay4me App</h3>
+        <div className="links">
+          <a href="*" style={{textDecoration: 'none', color: '#FFFFFF'}}>
+            <h3 style={{fontWeight: '700'}}>Home</h3>
+          </a>
+          <a href="*" style={{textDecoration: 'none', color: '#FFFFFF'}}>
+            <h3>About</h3>
+          </a>
+          <a href="*" style={{textDecoration: 'none', color: '#FFFFFF'}}>
+            <h3>Blog</h3>
+          </a>
+          <a href="*" style={{textDecoration: 'none', color: '#FFFFFF'}}>
+            <h3>Support</h3>
+          </a>
+        </div>
+        <button className='btn'><h3>Download App <img src={arrow} alt="" className= 'arrow' /></h3></button>
+      </nav>
+      <div className="head">
+        <h1>Simplifying Cross-Border Payments </h1>
+          <h1 className='hh'>for International Students</h1>
+          <span className='sp'>The easiest and fastest way to pay tuition and fees to educational</span>
+          <br />
+          <span className='s'>institutions, businessed and government agencies worldwide</span> <br />
+          <button className='bb'><h3>Get Started Now <img src={arrow} alt="" className='arr'/></h3></button> <br />
+          <div className="trust">
+            <span className='tr'>Trusted by 1000+ students</span>
+            <img src={first} alt="" className='first'/>
+            <img src={second} alt="" className='second'/>
+            <img src={third} alt="" className='third'/>
+            <img src={fourth} alt="" className='fourth'/>
+            <img src={fifth} alt="" className='fifth'/>
+          </div>
+      </div>
+      <div className="mid">
+        <div className="box">
+          <img src={wallet} alt="" className='wallet'/>
+          <p className='p'>Send payments quickly to universities, businesses or government agencies worldwide.</p>
+        </div>
+        <div className="glo">
+          <img src={arr} alt="" className='ar'/>
+          <img src={globe} alt=""  className='globe'/>
+          <img src={phone} alt="" className='phone'/>
+          <div className="overlay"></div>
+          <img src={arro} alt="" className='arro'/>
+        </div>
+        <div className="boxx">
+          <img src={empty} alt="" className='empty'/>
+          <p className='pp'>Easily add funds to your wallet using multiple payment methods</p>
+        </div>
+        <div className="group">
+          <img src={monnify} alt="" className='mon'/>
+          <img src={accross} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '13%', marginTop: '-.5%'}}/>
+          <img src={power} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '26%', marginTop: '-.5%'}}/>       
+          <img src={prodiy} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '39%', marginTop: '-.5%'}}/>   
+          <img src={stars} alt="" style={{position: 'absolute', width: '17vw', marginLeft: '52%', marginTop: '-.5%'}}/>
+        </div>
+        <div className="groupp">
+        <img src={serbit} alt="" className='mon'/>
+          <img src={vertio} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '12%', marginTop: '-.5%'}}/>
+          <img src={stripe} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '24%', marginTop: '-.5%'}}/>       
+          <img src={pay} alt="" style={{position: 'absolute', width: '13vw', marginLeft: '36%', marginTop: '-.5%'}}/>   
+          <img src={providus} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '52%', marginTop: '-1%'}}/>
+          <img src={smile} alt="" style={{position: 'absolute', width: '7vw', marginLeft: '62%', marginTop: '-.5%'}}/>
+        </div>
+        <div className="overlayr"></div>
+        <div className="overlayl"></div>
+      </div>
+      <div className="pagetwo">
+        <div className="top">
+          <h1 className='su'>Super fast tuition and</h1>
+          <h1 className='suu'>school fees payments</h1>
+          <div className="under"></div>
+          <p>Seamlessly pay local and international educational expenses</p>
+          <p>using our fast and convinient online payments platform from</p>
+          <p>the comfort of your home.</p>
+          <button className='button'><h3>Make A Payment <img src={arrow} alt="" className='a'/></h3></button>
+        </div>
+        <img src={group} alt="" style={{width: '30vw',marginTop: '4%',  marginLeft: '60%'}}/>
+        <img src={groupp} alt="" style={{width: '20vw', marginTop: '7%', marginLeft: '10%'}}/>
+        <div className="bottom">
+          <h1>Pay for SEVIS, WES, VISA,</h1>
+          <h1>Applications & More</h1>
+          <div className="underr"></div>
+          <p>We help you get closer to your study abroad and relocation</p>
+          <p>goals faster by promptly completing your payment orders for</p>
+          <p>credential evaluation, immigrtion, and visa purposes.</p>
+          <button className='butto'><h3>Make A Payment <img src={arrow} alt="" className='aa'/></h3></button>
+          </div>
+      </div>
+      <div className="pagethree">
+        <img src={starr} alt="" style={{width: '8vw', marginTop: '-5%', marginLeft: '91%', position: 'absolute'}}/>
+        <div className="side">
+          <h1>Trusted by</h1>
+          <h1>thousands of</h1>
+          <h1>students and</h1>
+          <h1>immigrants</h1>
+          <h1>around the</h1>
+          <h1>world</h1>
+          <div className="left"></div>
+        </div>
+        <img src={star} alt="" style={{width: '7vw'}}/>
+        <img src={frame} alt="" style={{width: '45vw', marginLeft: '35%', marginTop: '-21%', position:'absolute'}}/>
+      </div>
+      <div className="pagefour">
+        <div className="si">
+          <h1>FAQ</h1>
+          <p>Answers to your most pressing Pay4me App</p>
+          <p>Questions. We've got you!</p>
+          <img src={curly} alt="" style={{width: '25vw', marginLeft: '8%'}}/>
+        </div>
+        <div className="boxes">
+          <div className="con">
+            <h3>What is the Pay4me App?</h3>
+            <img src={minus} alt="" className='minus'/>
+            <p>Pay4Me App is a platform that helps people across Africa complete</p>
+            <p>cros-border payments services. Our core payment industry</p>
+            <p>includes helping with payments to government and immigration</p>
+            <p>agencies, cookies, universities, businesses, and organizations</p>
+            <p>abroad</p>
+          </div>
+          <div className="conn">
+            <h3>What is the Payment Processing Timeline of the Pay4me App?</h3>
+            <img src={plus} alt="" style={{marginLeft: '93%', marginTop: '-7.3%', position: 'absolute'}}/>
+          </div>
+          <div className="connn">
+          <h3>How does the Pay4me App work?</h3>
+          <img src={plus} alt="" style={{marginLeft: '93%', marginTop: '-7.3%', position: 'absolute'}}/>
+          </div>
+          <div className="co">
+          <h3>How do i get started with the Pay4me App?</h3>
+          <img src={plus} alt="" style={{marginLeft: '93%', marginTop: '-7.3%', position: 'absolute'}}/>
+          </div>
+        </div>
+
+        <img src={back} alt="" style={{width: '15vw', opacity: '200%', filter: 'blur(3px)'}}/>
+      </div>
+      <div className="pagefive">
+        <img src={contact} alt="" className='contact'/>
+      </div>
+      <div className="pagesix">
+        <div className="lat">
+          <h1>Latest Blog &</h1>
+          <h1>Resources</h1>
+          <p style={{marginTop: '3%'}}>Your Go-To Guide fro Pay4Me App:</p>
+          <p>Answers to All Your Questions!</p>
+          <button className='b'><h3>View All <img src={arrow} alt="" className='aaa'/></h3></button>
+        </div>
+        <div className="im">
+          <img src={woman} alt="" style={{width: '20vw'}}/>
+          <p>How to Navigate International Payments</p>
+          <p>for Education Without Stress</p>
+          <span>Studying abroad can be an exciting</span><br />
+          <span>journey, but it comes with its fair share...</span>
+          <button className='rm'><h3>Read More <img src={arrow} alt="" className='na'/></h3></button>
+        </div>
+        <div className="imm">
+        <img src={man} alt="" style={{width: '20vw'}}/>
+          <p>Overcoming Challenges in Foreign</p>
+          <p>Currecu Tuition Payments</p>
+          <span>Navigating foreign currency tuition</span><br />
+          <span>payment options can be daunting for...</span>
+          <button className='rm'><h3>Read More <img src={arrow} alt="" className='na'/></h3></button>
+        </div>
+        <div className="ba">
+          <button className='baa'><img src={ar} alt="" /></button>
+          <button className='bab'><img src={arw} alt="" /></button>
+        </div>
+      </div>
+      <div className="last">
+      <div className="inner">
+      <img src={back} alt="" className='ll'/>
+       <h1>Download The Pay4Me App</h1>
+        <p>Our Custom QR cide will take you to your app store to download <br /> our awesome native apps</p>
+        <img src={google} alt="" style={{width: '8vw', marginTop: '3%'}}/>
+        <img src={app} alt="" style={{width: '8vw'}}/>
+        <img src={QR} alt="" className='qr'/>
+        <img src={phoeee} alt="" className='ph'/>
+      </div>
+      <div className="bot">
+        <img src={logo} alt="" />
+        <h3 style={{marginLeft: '3%', marginTop: '-2.3%'}}>Pay4Me App</h3>
+        <p>© 2020-2024 Pay4me, Inc.</p>
+        <h3 style={{marginLeft: '40%', marginTop: '-5.1%'}}>Company</h3>
+        <div className="ffp">
+        <p>About</p>
+        <p>Blog</p>
+        <p>FAQs</p>
+        <p>Support</p>
+        <h3  style={{marginLeft: '20%', marginTop: '-27%'}}>Legal</h3>
+        <div className="sp">
+          <p>Terms of Service</p>
+          <p>Order Cancellation Policy</p>
+          <p>Privacy Policy</p>
+          <p>KYC/AML Policy</p>
+          <h3  style={{marginLeft: '40%', marginTop: '-36%'}}>Resources</h3>
+          <div className="tp">
+            <p>Andriod Reviews </p>
+            <p>IOS Reviews</p>
+            <p>Jobs</p>
+            <p> Testimonials</p>
+          </div>
+        </div>
+        </div>
+        <img src={logos} alt="" style={{width: '7vw', marginTop: '-2.5%', position: 'absolute'}}/>
+      </div>
+      <img src={payme} alt="" style={{width: '60vw', marginTop: '5%', marginLeft: '19%'}}/>
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
